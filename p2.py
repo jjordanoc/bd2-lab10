924bee422b301fbe5e5372cf98211a5fa2fbe88a
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2023-06-22 13:17:15 -0500

    feat: finished p2

--- p2.py
+++ p2.py
@@ -1,11 +1,16 @@
 import os
 import random
+import matplotlib.pyplot as plt
+import face_recognition
+import numpy as np
 
 """
-Load random sample (N=5000)
+Load random sample
 """
 
 path = 'C:/Users/rojot/OneDrive/Escritorio/bd2/repo/lab10/lfw'  # replace
+
+N = 5000
 
 faces = []
 
@@ -14,10 +19,30 @@ for subdir, dirs, files in os.walk(path):
         face_path = os.path.join(subdir, file)
         faces.append(face_path)
 
-faces_to_compare = random.sample(faces, 5000)
-
-print(faces_to_compare)
+faces_to_compare = random.sample(faces, N)
 
 """
 Calculate embeddings within samples
 """
+
+face_embeddings = []
+
+for face_file in faces_to_compare:
+    image = face_recognition.load_image_file(face_file)
+    embedding_list = face_recognition.face_encodings(image)
+    if len(embedding_list) > 0:
+        face_embeddings.append(embedding_list[0])
+
+"""
+Calculate euclidean distance
+"""
+
+x = []
+
+for img1_embeds in face_embeddings:
+    for img2_embeds in face_embeddings:
+        dist = np.linalg.norm(img1_embeds - img2_embeds)  # euclidean distance
+        x.append(dist)
+
+plt.hist(x)
+plt.show()
